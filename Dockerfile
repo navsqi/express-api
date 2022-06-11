07767bd1becf0ad8cbb68a0f29b5a655f43fc5c2
aprilliaazizah <aprilliaazizah3@gmail.com>
2022-06-11 14:56:27 +0700

    dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 # STAGE 1
 FROM node:14.17.5-alpine AS build
+ENV PORT=5000
 ENV NODE_ENV=production
+ENV NAMA="NAUVAL SHIDQI"
 # Add a work directory
 WORKDIR /app
 # Cache and Install dependencies
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // export module
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello world!"));
+app.get("/", (req, res) => res.send("Hello world! " + process.env.NAMA));
 
 module.exports = app;
